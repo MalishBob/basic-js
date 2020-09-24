f749182c732a41bc3ea22751fb0571796431e106
Azad <azad_63_mamedov@mail.ru>
2020-09-24 23:56:03 +0400

    init: add count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,21 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if(typeof sampleActivity !== 'string' 
+    || isNaN(parseFloat(sampleActivity)) 
+    || /^[\-a-zA-Z]\.{2,}$/.test(sampleActivity) 
+    || parseFloat(sampleActivity)>MODERN_ACTIVITY  
+    || parseFloat(sampleActivity) <= 0){
+       return false; 
+  }
+
+  let A = parseFloat(sampleActivity);
+  let A_0 = MODERN_ACTIVITY;
+  let t_05 = HALF_LIFE_PERIOD;
+  let k = 0.693/t_05;
+  let t = Math.ceil(Math.log(A_0/A)/k);
+
+  return t;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  // throw new CustomError('Not implemented');
+
+  let catsCount = 0;
+  if(matrix && matrix.length>0){
+    for(i=0; i<matrix.length; i++){
+      for(j=0; j<matrix[i].length; j++){
+        if(matrix[i][j] === '^^'){
+          catsCount++;
+        }
+      }
+    }
+  } else {
+    return 0;
+  }
+  return catsCount;
+  
 };
