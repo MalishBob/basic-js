fde75c326637c678ee76aded08a0180a73543cb9
Azad <azad_63_mamedov@mail.ru>
2020-10-04 00:51:25 +0400

    feat: solve dream-team, what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if(typeof(members) === null || typeof(members) === 'undefined' || !Array.isArray(members) || members.length === 0) return false;
+
+  let dreamTeamNameArr = [];
+  let dreamTeamName = '';
+
+  for (let i = 0; i < members.length; i++) {
+    if(typeof(members[i]) !== "string") continue;
+    dreamTeamNameArr.push(members[i].toUpperCase().trim().charAt(0));
+  }
+
+  dreamTeamNameArr.sort();
+  dreamTeamName = dreamTeamNameArr.join('');
+
+  return dreamTeamName;
 };
